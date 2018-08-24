dfaa2c536506554ca9c82dba0be695f5b40c5d0e
Adrian Stoll <adrs@umich.edu>
2018-08-24 00:50:24 -0400

    minimum viable product: dump sms log to json

--- app/src/main/java/com/adrianstoll/exportsms/MainActivity.java
+++ app/src/main/java/com/adrianstoll/exportsms/MainActivity.java
@@ -3,18 +3,25 @@ package com.adrianstoll.exportsms;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Environment;
 import android.provider.Telephony;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonWriter;
 import android.util.Log;
 import android.view.View;
 import android.provider.Telephony.Sms;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-	private final static int REQUEST_SMS_READ_PERMISSION = 42;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,59 +29,123 @@ public class MainActivity extends AppCompatActivity {
     }
     // TODO: move to utils
     private void showMessage(String msg) {
+		Log.d("adrs", msg);
+		// TODO: show messages that requires user interaction before going away
 		Toast toast = Toast.makeText(
 				getApplicationContext(),
 				msg,
 				Toast.LENGTH_LONG);
 		toast.show();
 	}
-    @Override
+	private boolean isExternalStorageWritable() {
+		return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+	}
+	private File getExportFile(String name) {
+    	// TODO: check that external storage is available
+		if(!this.isExternalStorageWritable()) {
+			Log.d("adrs", "external storage is not writable");
+			return null;
+		}
+    	File file = new File(Environment.getExternalStoragePublicDirectory(
+    			Environment.DIRECTORY_DOWNLOADS),name);
+		return file;
+	}
+	private final static String[] REQUIRED_PERMISSIONS = {
+			Manifest.permission.READ_SMS,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE
+	};
+    private int numPermissionsGranted = 0;
+
+
+	@Override
     public void onRequestPermissionsResult(int requestCode,
 										   String permissions[],
 										   int[] grantResults) {
-    	// Check that we have an actual result
-    	if(requestCode != MainActivity.REQUEST_SMS_READ_PERMISSION &&
-				grantResults.length == 0) {
-    		return;
+		for(int i = 0; i < permissions.length; ++i) {
+			if(grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+				Log.d("adrs", permissions[i] + " permission granted");
+				this.numPermissionsGranted++;
+			} else {
+				this.showMessage(permissions[i] + " permission must be granted to export messages");
+			}
 		}
-		if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-    		Log.d("adrs", "READ_SMS permission granted");
+		Log.d("adrs", "num perms granted: " + Integer.valueOf(this.numPermissionsGranted));
+		// Only continue when all request permissions have been granted
+		if(this.numPermissionsGranted == REQUIRED_PERMISSIONS.length) {
 			this.exportSMS();
-		} else {
-			Log.d("adrs", "READ_SMS permission not granted");
-			this.showMessage("READ_SMS permission must be granted to export messages");
 		}
 	}
+	public void dumpToJson(Cursor cur, OutputStreamWriter stream) throws IOException {
+		JsonWriter writer = new JsonWriter(stream);
+
+		// Write array of messages
+		writer.beginArray();
+
+		// see: https://developer.android.com/reference/android/provider/Telephony.TextBasedSmsColumns#MESSAGE_TYPE_ALL
+		// TODO: fetch names from contacts
+		while(cur.moveToNext()) {
+			// Write message object
+			//Log.d("adrs","{");
+			writer.beginObject();
+			int numCols = cur.getColumnCount();
+			for(int i = 0; i < numCols; ++i) {
+				String name = cur.getColumnName(i);
+				String value = cur.getString(i);
+				//Log.d("adrs", "\t" + name + ": " + value);
+				writer.name(name).value(value);
+			}
+			//Log.d("adrs","}");
+			writer.endObject();
+		}
+		cur.close();
+		writer.endArray();
+		writer.close();
+		Log.d("adrs","finished export");
+	}
 	public void exportSMS() {
-		Log.d("adrs", "Exporting SMS messages...");
-		Log.d("adrs", Telephony.Sms.CONTENT_URI.toString());
-		// Check if we have the permission to read SMS messages
-		if(ContextCompat.checkSelfPermission(this,
-				Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
-			ActivityCompat.requestPermissions(this,
-					new String[]{Manifest.permission.READ_SMS},
-					MainActivity.REQUEST_SMS_READ_PERMISSION);
-			Log.d("adrs", "Asked for permission");
+		// Make sure we have the proper permissions
+		// See what permissions were're missing
+		this.numPermissionsGranted = 0;
+		ArrayList<String> ungrantedPermissions = new ArrayList<String>();
+		for(int i = 0; i < REQUIRED_PERMISSIONS.length; ++i) {
+			if(ContextCompat.checkSelfPermission(this,
+					REQUIRED_PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
+				ungrantedPermissions.add(REQUIRED_PERMISSIONS[i]);
+			} else {
+				this.numPermissionsGranted++;
+			}
+		}
+		// Ask for ungranted permissions
+		if(this.numPermissionsGranted != REQUIRED_PERMISSIONS.length) {
+			Log.d("adrs", "asking for permissions");
+			ActivityCompat.requestPermissions(this, ungrantedPermissions.toArray(new String[0]), 0);
+			Log.d("adrs", "Asked for permissions");
 			return;
 		}
-		Log.d("adrs", "Have permission");
+
+		// Get a cursor into the SMS message database
 		Cursor messageCursor = getContentResolver().query(
 				Telephony.Sms.CONTENT_URI,
 				null,
 				null,
 				null,
 				null);
-		int max = 25;
-		int cur = 0;
-		while(messageCursor.moveToNext() && cur < max) {
-			int numCols = messageCursor.getColumnCount();
-			for(int i = 0; i < numCols; ++i) {
-				Log.d("adrs",
-						messageCursor.getColumnName(i) + ": " + messageCursor.getString(i));
-			}
-			++cur;
+
+		// Create file to export messages to
+		File exportFile = this.getExportFile("ExportSms.json");
+		if(exportFile.exists()) {
+			this.showMessage("Export file in downloads already exists. Aborting.");
+			return;
 		}
-		messageCursor.close();
+		Log.d("adrs", "About to dump");
+		// Dump messages to json file
+		try {
+			this.dumpToJson(messageCursor, new OutputStreamWriter(new FileOutputStream(exportFile), "UTF-8"));
+		} catch (IOException e) {
+			this.showMessage("could not export messages: " + e.toString());
+			return;
+		}
+		this.showMessage("Finished Export");
 	}
     public void exportHandler(View view) {
         exportSMS();
